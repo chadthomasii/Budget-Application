1743e363058e4d46aef93be3aad3f32a49648c60
Chad Thomas <chad.thomas@avalara.com>
2017-10-13 10:46:44 -0400

    Added necessary files for application, changed app colors.

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Aggie Budget</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="shortcut icon" href="img/JuggLogo.png" />
+        <link rel="stylesheet" href="./font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+
+        <header id="showcase">
+
+            <div class="heading">
+               <div>
+                   <h1></h1>
+                   <h1>Weekly Balance: $400</h1>
+               </div>
+            </div>
+
+            
+        </header>
+
+        <div class="card-container">
+            <div class="card-border">
+                <section class="card">
+                    <div class="card-heading">
+                        <i class="fa fa-user fa-4x" aria-hidden="true"></i>
+                        <h1>Using Jugg</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et odio tincidunt, suscipit tellus id, molestie ipsum. 
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <section>More</section>
+                    </div>
+                </section>
+
+            </div>
+
+            <div class="card-border">
+
+                <section class="card">
+                    <div class="card-heading">
+                        <i class="fa fa-lock fa-4x" aria-hidden="true"></i>
+                        <h1>Terms & Policies</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et odio tincidunt, suscipit tellus id, molestie ipsum. 
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <section>More</section>
+                    </div>
+
+                </section>
+
+            </div>
+
+            <div class="card-border">
+                <section class="card">
+                    <div class="card-heading">
+                        <i class="fa fa-question fa-4x" aria-hidden="true"></i>
+                        <h1>FAQ</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et odio tincidunt, suscipit tellus id, molestie ipsum. 
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <section>More</section>
+                    </div>
+
+                </section>
+
+            </div>
+            
+        </div>
+
+      
+        
+    </body>
+</html>
